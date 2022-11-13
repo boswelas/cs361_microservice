e028403c2f03e48f08982c47ce195a6e7e574cdd
boswelas <71548011+boswelas@users.noreply.github.com>
2022-11-13 18:17:35 +0900

    updated to run on zmq

--- random_number.py
+++ random_number.py
@@ -1,18 +1,14 @@
-import time
 import random
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
 
 
-def random_number_generator(pathname):
-    while True:
-        time.sleep(1)
-        f = open(pathname, 'r+')
-        text = f.readline()
-
-        if text == 'run':
-            random_num = str(random.randint(0, 2))
-            f.seek(0)
-            f.truncate(0)
-            f.write(random_num)
-
-        f.close()
-
+while True:
+    text = socket.recv(100).decode()
+    print(text)
+    if text == 'run':
+        random_num = str(random.randint(0, 2))
+        socket.send(random_num.encode())
